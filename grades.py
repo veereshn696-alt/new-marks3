c461119af97b5c46b8f9979e0f8eea1d986f1bf5
veereshn696-alt <veereshn696@gmail.com>
2025-10-23 16:26:17 +0530

    hlo

---
+++ grades.py
@@ -0,0 +1,12 @@
+marks=int(input("enter the marks :"))
+i=0
+if marks>90 :
+    grade="a"
+elif marks>80 :
+    grade="b"
+elif marks>70 :
+    grade ="c"
+elif marks>60 :
+    grade="d"
+else :
+    grade="fail"
